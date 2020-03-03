2582bed96951fc6497c9983a00f71de6f695bcf5
王和建 <wanghejian@weile.com>
2020-03-03 18:03:49 +0800

    submissions

---
+++ leetcode/valid-parentheses/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func isValid(s string) bool {
+	l := list.New()
+	for _, v := range s {
+		switch v {
+		case '(':
+			l.PushBack(v)
+		case '{':
+			l.PushBack(v)
+		case '[':
+			l.PushBack(v)
+		case ')':
+			if l.Len() == 0 {
+				return false
+			}
+
+			x := l.Back()
+			xx := x.Value.(rune)
+			if xx != '(' {
+				return false
+			}
+			l.Remove(x)
+		case '}':
+			if l.Len() == 0 {
+				return false
+			}
+
+			x := l.Back()
+			xx := x.Value.(rune)
+			if xx != '{' {
+				return false
+			}
+			l.Remove(x)
+		case ']':
+			if l.Len() == 0 {
+				return false
+			}
+
+			x := l.Back()
+			xx := x.Value.(rune)
+			if xx != '[' {
+				return false
+			}
+			l.Remove(x)
+		default:
+		}
+	}
+
+	if l.Len() != 0 {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(isValid("(1)"))
+	fmt.Println(isValid(""))
+	fmt.Println(isValid(" "))
+	fmt.Println(isValid("(1[)]"))
+	fmt.Println(isValid("([{232}])"))
+	fmt.Println(isValid("("))
+	fmt.Println(isValid(")"))
+}
